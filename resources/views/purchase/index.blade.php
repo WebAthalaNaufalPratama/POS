6022f982447286b0a86abdee32297476ec6becf9
19Hamid91 <99725605+19Hamid91@users.noreply.github.com>
2024-07-05 16:37:11 +0700

    update: acc pembelian inadmin gallery

--- resources/views/purchase/index.blade.php
+++ resources/views/purchase/index.blade.php
@@ -9,9 +9,11 @@
                     <div class="page-title">
                         <h4>Purchase Order</h4>
                     </div>
+                    @role('Purchasing')
                     <div class="page-btn">
                         <a href="{{ route('pembelian.create') }}" class="btn btn-added"><img src="/assets/img/icons/plus.svg" alt="img" class="me-1" />Tambah Pembelian</a>
                     </div>
+                    @endrole
                 </div>
             </div>
             <div class="card-body">
@@ -61,9 +63,15 @@
                                 <th>Tanggal Terima</th>
                                 <th>No DO Supplier</th>
                                 <th>Lokasi</th>
+                                @role('Purchasing')
                                 <th>Status Purchase</th>
-                                {{-- <th>Status Admin</th>
-                                <th>Status Finance</th> --}}
+                                @endrole
+                                @role('AdminGallery')
+                                <th>Status Admin</th>
+                                @endrole
+                                @role('Finance')
+                                <th>Status Finance</th>
+                                @endrole
                                 <th>Status Pembayaran</th>
                                 <th>Barang Retur</th>
                                 <th>Aksi</th>
@@ -79,10 +87,15 @@
                                 <td>{{ $datapo->tgl_diterima ? tanggalindo($datapo->tgl_diterima) : ''}}</td>
                                 <td>{{ $datapo->no_do_suplier}}</td>
                                 <td>{{ $datapo->lokasi->nama}}</td>
+                                @role('Purchasing')
                                 <td>{{ $datapo->status_dibuat}}</td>
-                                {{-- <td>{{ $datapo->status_diterima}}</td>
-                                <td>{{ $datapo->status_diperiksa}}</td> --}}
-
+                                @endrole
+                                @role('AdminGallery')
+                                <td>{{ $datapo->status_diterima}}</td>
+                                @endrole
+                                @role('Finance')
+                                <td>{{ $datapo->status_diperiksa}}</td>
+                                @endrole
                                 <td>
                                 @if ($datapo->invoice !== null && $datapo->invoice->sisa == 0 )
                                     LUNAS
@@ -98,7 +111,7 @@
                                 @endif
                                 </td>
                                 
-                            
+                                @role('Purchasing')
                                 <td class="text-center">
                                     <a class="action-set" href="javascript:void(0);" data-bs-toggle="dropdown" aria-expanded="true">
                                         <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
@@ -127,19 +140,27 @@
                                         <li>
                                             <a href="{{ route('pembelian.show', ['type' => 'pembelian','datapo' => $datapo->id]) }}" class="dropdown-item"><img src="/assets/img/icons/eye1.svg" class="me-2" alt="img">Detail PO</a>
                                         </li>
-                                        @if ($datapo->tgl_diterima_ttd === null)
-                                        <li>
-                                            <a href="{{ route('pembelian.edit', ['type' => 'pembelian','datapo' => $datapo->id]) }}" class="dropdown-item"><img src="/assets/img/icons/edit.svg" class="me-2" alt="img">Acc Terima</a>
-                                        </li>
-                                        @endif
                                         <li>
                                             <a href="{{ route('pembelian.editpurchase', ['type' => 'pembelian','datapo' => $datapo->id]) }}" class="dropdown-item"><img src="/assets/img/icons/edit.svg" class="me-2" alt="img">Edit PO</a>
                                         </li>
+                                    </ul>
+                                </td>
+                                @endrole
+                                @role('AdminGallery')
+                                <td class="text-center">
+                                    <a class="action-set" href="javascript:void(0);" data-bs-toggle="dropdown" aria-expanded="true">
+                                        <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+                                    </a>
+                                    <ul class="dropdown-menu">
                                         <li>
-                                            {{-- <a href="#" class="dropdown-item" onclick="deleteData({{ $datapo->id }})"><img src="/assets/img/icons/delete1.svg" class="me-2" alt="img">Delete</a> --}}
+                                            <a href="{{ route('pembelian.show', ['type' => 'pembelian','datapo' => $datapo->id]) }}" class="dropdown-item"><img src="/assets/img/icons/eye1.svg" class="me-2" alt="img">Detail PO</a>
+                                        </li>
+                                        <li>
+                                            <a href="{{ route('pembelian.edit', ['type' => 'pembelian','datapo' => $datapo->id]) }}" class="dropdown-item"><img src="/assets/img/icons/edit.svg" class="me-2" alt="img">Acc Terima</a>
                                         </li>
                                     </ul>
                                 </td>
+                                @endrole
 
                             </tr>
                             @endforeach
