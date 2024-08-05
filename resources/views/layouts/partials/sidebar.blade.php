b0483489b414573e25b04a85e9d087d48857d1a4
WebAthalaNaufalPratama <attagledek546@gmail.com>
2024-08-05 15:33:38 +0700

    testting

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -197,7 +197,7 @@
                         <li><a href="{{ route('laporan.dopenjualan') }}" class="{{ request()->is('laporan/dopenjualan*') ? 'active' : '' }}">Delivery Order</a></li>
                         <li><a href="{{ route('laporan.returpenjualan') }}" class="{{ request()->is('laporan/returpenjualan*') ? 'active' : '' }}">Retur Penjualan</a></li>
                         <li><a href="{{ route('laporan.penjualan') }}" class="{{ request()->is('laporan/penjualan') ? 'active' : '' }}">Penjualan</a></li>
-                        <li><a href="{{ route('laporan.mutasi') }}" class="{{ request()->is('laporan/mutasi*') ? 'active' : '' }}">Mutasi</a></li>
+                        <li><a href="{{ route('laporan.mutasi') }}" class="{{ request()->is('laporan/mutasi') ? 'active' : '' }}">Mutasi</a></li>
                         <li><a href="{{ route('laporan.mutasiinden') }}" class="{{ request()->is('laporan/mutasiinden*') ? 'active' : '' }}">Mutasi Inden</a></li>
                         <li><a href="{{ route('laporan.kas_pusat') }}" class="{{ request()->is('laporan/kas_pusat*') ? 'active' : '' }}">Kas Pusat</a></li>
                         <li><a href="{{ route('laporan.kas_gallery') }}" class="{{ request()->is('laporan/kas_gallery*') ? 'active' : '' }}">Kas Gallery</a></li>
